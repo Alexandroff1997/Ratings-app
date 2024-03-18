d3d059f06e44d68c47b0afbbde31cb95a70b3a8b
Alexander <symire.alexander1997@gmail.com>
2024-03-19 06:57:49 +0700

    init app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-page-custom-font */
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return <>
+    <Component {...pageProps} />;
+  </>;
 }
