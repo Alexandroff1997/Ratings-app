119b7ec7678fea8d4ecdd1bca9b26450484e09bb
Alexander <symire.alexander1997@gmail.com>
2024-03-19 08:27:16 +0700

    button types and dynamic props

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,14 @@ import styles from './Button.module.css';
 import { IButtonProps } from './Button.props';
 import cn from 'classnames';
 
-export const Button = ({ appearance, children }: IButtonProps): JSX.Element => {
+export const Button = ({ appearance, children, className, ...props }: IButtonProps): JSX.Element => {
 	return (
 		<button
-			className={cn(styles.button, {
+			className={cn(styles.button, className, {
 				[styles.primary]: appearance === 'primary',
 				[styles.ghost]: appearance === 'ghost'
 			})}
+			{...props}
 		>
 			{children}
 		</button>
